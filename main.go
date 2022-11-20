c95eb77c7179c345620dadd762572f32c82e7471
ju570r1u5 <albertodeprezzo@gmail.com>
2022-11-20 22:34:33 +0100

    add printing utils

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"log"
+
 	"github.com/deeper-x/go-dsa/binarysearch"
 	"github.com/deeper-x/go-dsa/linearsearch"
 )
 
-var needle = 10
+// DataSearch result object
+type DataSearch struct {
+	Name   string
+	Result bool
+}
+
+var needle = 11
 var haystack = []int{1, 5, 6, 7, 8, 9, 10, 11, 13, 15}
 
 func main() {
-	linearsearch.Run(needle, haystack)
-	binarysearch.Run(needle, haystack)
+	ls := NewData("Linear Search", linearsearch.Run(needle, haystack))
+	bs := NewData("Binary Search", binarysearch.Run(needle, haystack))
+
+	printSearch(ls)
+	printSearch(bs)
+}
+
+// printSearch data printing utility
+func printSearch(ds DataSearch) {
+	log.Printf("%s: %t", ds.Name, ds.Result)
+}
+
+// NewData builder
+func NewData(name string, result bool) DataSearch {
+	return DataSearch{
+		Name:   name,
+		Result: result,
+	}
 }
